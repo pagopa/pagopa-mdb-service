ee4b7ba6cd490486c3218eb456fbab53d7a80f97
svariant <samuele.varianti@nttdata.com>
2024-12-02 12:16:14 +0100

    [PPANTT-190] chore: Configure GitHub workflows

--- .github/workflows/update_gha.py
+++ .github/workflows/update_gha.py
@@ -1,16 +1,17 @@
-import json
 import requests
+import json
 
-url = 'https://api.github.com/repos/pagopa/template-java-spring-microservice/contents/.github/workflows'
+
+url = 'https://github.com/pagopa/template-java-spring-microservice/tree/main/.github/workflows'
 url_raw = 'https://raw.githubusercontent.com/pagopa/template-java-spring-microservice/main/'
 
 response = requests.get(url)
 
-for item in json.loads(response.text):
+for item in json.loads(response.text)["payload"]["tree"]["items"]:
     path = item["path"]
     name = item["name"]
     print(name)
-    response = requests.get(url_raw + path)
+    response = requests.get(url_raw+path)
     fo = open(name, "w")
     fo.write(response.text)
     fo.close()
