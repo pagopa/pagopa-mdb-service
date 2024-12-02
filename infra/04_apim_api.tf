83ee5e92fce85b6a29cbb1852289d348e2223fda
svariant <samuele.varianti@nttdata.com>
2024-12-02 12:45:14 +0100

    [PPANTT-190] chore: Configure Infra

--- infra/04_apim_api.tf
+++ infra/04_apim_api.tf
@@ -1,56 +1,54 @@
 locals {
-  repo_name = "TODO" # TODO add the name of the repository
-
-  display_name = "TODO" # TODO
-  description  = "TODO" # TODO
-  path  = "TODO" # TODO add your base path
-
-  host         = "api.${var.apim_dns_zone_prefix}.${var.external_domain}"
-  hostname     = var.hostname
+  apim_mbd = {
+    // GPD Payments Pull
+    display_name          = "Marca da Bollo Digitale"
+    description           = "API for eBollo 2.0"
+    path                  = "mbd/service"
+    subscription_required = true
+    service_url           = null
+  }
+  host     = "api.${var.apim_dns_zone_prefix}.${var.external_domain}"
+  hostname = var.hostname
 }
 
-resource "azurerm_api_management_group" "api_group" {
-  name                = local.apim.product_id
-  resource_group_name = local.apim.rg
-  api_management_name = local.apim.name
-  display_name        = local.display_name
-  description         = local.description
-}
+##############
+## Api Vers ##
+##############
 
-resource "azurerm_api_management_api_version_set" "api_version_set" {
-  name                = format("%s-${local.repo_name}", var.env_short)
+resource "azurerm_api_management_api_version_set" "api_mbd_service" {
+
+  name                = format("%s-ebollo-mbd-service-api", var.env_short)
   resource_group_name = local.apim.rg
   api_management_name = local.apim.name
-  display_name        = local.display_name
+  display_name        = local.apim_mbd.display_name
   versioning_scheme   = "Segment"
 }
 
-module "api_v1" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.7.0"
+##############
+## OpenApi  ##
+##############
 
-  name                  = format("%s-${local.repo_name}", var.env_short)
+module "apim_mbd_api_v1" {
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//api_management_api?ref=v6.4.1"
+
+  name                  = format("%s-mbd-api", var.env_short)
   api_management_name   = local.apim.name
   resource_group_name   = local.apim.rg
   product_ids           = [local.apim.product_id]
-  subscription_required = true
+  subscription_required = local.apim_mbd.subscription_required
+  version_set_id        = azurerm_api_management_api_version_set.api_mbd_service.id
+  api_version           = "v1"
 
-  version_set_id = azurerm_api_management_api_version_set.api_version_set.id
-  api_version    = "v1"
-
-  description  = local.description
-  display_name = local.display_name
-  path         = local.path
+  description  = local.apim_mbd.description
+  display_name = local.apim_mbd.display_name
+  path         = local.apim_mbd.path
   protocols    = ["https"]
-
-  service_url = null
+  service_url  = local.apim_mbd.service_url
 
   content_format = "openapi"
-  content_value  = templatefile("../openapi/openapi.json", {
-    host = local.host
-  })
+  content_value  = file("../openapi/openapi.json")
 
   xml_content = templatefile("./policy/_base_policy.xml", {
-    hostname = var.hostname
+    hostname = local.hostname
   })
 }
-
--- infra/99_locals.tf
+++ infra/99_locals.tf
@@ -2,9 +2,8 @@ locals {
   product = "${var.prefix}-${var.env_short}"
 
   apim = {
-    name       = "${local.product}-apim"
-    rg         = "${local.product}-api-rg"
-    product_id = "TODO" # TODO product id to import from pagopa-infra
+    name                   = "${local.product}-apim"
+    rg                     = "${local.product}-api-rg"
+    product_id = "pagopa_mbd"
   }
 }
-
