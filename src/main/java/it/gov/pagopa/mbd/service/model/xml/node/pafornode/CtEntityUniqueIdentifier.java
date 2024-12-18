b9dd9889f3c163434eada9ca916951c9f0ca4e60
Alessio Cialini <63233981+alessio-cialini@users.noreply.github.com>
2024-12-18 13:01:56 +0100

    Update src/main/java/it/gov/pagopa/mbd/service/model/xml/node/pafornode/CtEntityUniqueIdentifier.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/it/gov/pagopa/mbd/service/model/xml/node/pafornode/CtEntityUniqueIdentifier.java
+++ src/main/java/it/gov/pagopa/mbd/service/model/xml/node/pafornode/CtEntityUniqueIdentifier.java
@@ -1,8 +1,8 @@
 //
-// Questo file è stato generato dall'Eclipse Implementation of JAXB, v3.0.0 
-// Vedere https://eclipse-ee4j.github.io/jaxb-ri 
-// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2024.12.18 alle 10:59:02 AM CET 
+// Questo file è stato generato dall'Eclipse Implementation of JAXB, v3.0.0
+// Vedere https://eclipse-ee4j.github.io/jaxb-ri
+// Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine.
+// Generato il: 2024.12.18 alle 10:59:02 AM CET
 //
 
 
